5d03ce451f909188a61526fc3b40f675e154a9bc
pptichka <taleskina@gmail.com>
2022-09-17 17:48:46 +0300

    Initial Commit

---
+++ TrafficLightApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrafficLightApp
+//
+//  Created by Ксн Тлскн on 17.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
